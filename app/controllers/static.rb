a1764fbb05710475ca14edf15ed24203532f61a1
coderamen <spencerteh@gmail.com>
2017-02-13 20:50:12 +0800

    login / logout

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -2,15 +2,35 @@ get '/' do
   erb :"static/index"
 end
 
+get '/signup' do
+  erb :"static/signup"
+  # user = User.new(params[:user])
+  # if user.save
+  #   redirect "/su"
+  # else
+  #   # what should happen if the user keyed in invalid date?
+  # end
+end
+
 post '/signup' do
-  user = User.new(params[:user])
+  user = User.new(email: params[:email], user_name: params[:user_name], password: params[:password])
   if user.save
-    # what should happen if the user is saved?
+    redirect "/signup/success"
   else
-    # what should happen if the user keyed in invalid date?
+    erb :"static/signup"
   end
-end  
+end
 
+get '/signup/success' do
+  erb :"static/success"
+end
 
-
-
+post '/signin' do
+  user = User.find_by(email: params[:email])
+  if user.authenticate(params[:password])
+    session[:user_id] = user.id
+    erb :"static/home"
+  else
+    'invalid login'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many: questions
-  has_many: answers
   has_secure_password
+  validates :email, :user_name, presence: true
+  validates :email, :user_name, uniqueness: true
 end
