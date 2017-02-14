14f4c29ade949b0f81ece9a5aae2a611423992d5
coderamen <spencerteh@gmail.com>
2017-02-14 13:28:14 +0800

    signin routes fix from display logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-post '/sessions/logout' do #sign in / direct to action
+post '/sessions' do #sign in / direct to action
   user = User.find_by(email: params[:email])
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
