42d8e85779f851c970fcfdeddb7c72b4010d5ae0
coderamen <spencerteh@gmail.com>
2017-02-17 07:51:57 +0800

    working but can click once per user

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,17 +3,17 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
 
-def vote_count
-    votes = Vote.where(question_id: @question.id)
-    vote_count = 0
-    votes.each do |v|
-      if v.vote
-        vote_count += 1
-      else
-        vote_count -= 1
+  def vote_count
+      votes = QuestionVote.where(question_id: self.id)
+      vote_count = 0
+      votes.each do |v|
+        if v.vote
+          vote_count += 1
+        else
+          vote_count -= 1
+        end
       end
       vote_count
-    end
-end
+  end
 
 end
