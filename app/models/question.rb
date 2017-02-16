a8230b84cf0a47e301f4b3063c13bd59b9545b58
coderamen <spencerteh@gmail.com>
2017-02-17 00:21:15 +0800

    WIP vote p2

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,18 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
+
+def vote_count
+    votes = Vote.where(question_id: @question.id)
+    vote_count = 0
+    votes.each do |v|
+      if v.vote
+        vote_count += 1
+      else
+        vote_count -= 1
+      end
+      vote_count
+    end
+end
+
 end
