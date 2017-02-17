b11c0401d020d9f96675f29560cdbc8a5135fbce
coderamen <spencerteh@gmail.com>
2017-02-17 19:39:24 +0800

    vote feature 1. can vote 1 question per user 2. can vote other user question once

--- app/models/question_vote.rb
+++ app/models/question_vote.rb
@@ -1,6 +1,6 @@
 
 class QuestionVote < ActiveRecord::Base
-validates_uniqueness_of :user_id, :question_id
+validates_uniqueness_of :user_id, :scope => :question_id
   belongs_to :user
   belongs_to :question
 end
